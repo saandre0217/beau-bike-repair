f3845d61d66e4bb1c07ce5a889ef9c6a5645e5e1
Sydney Andre <s.a.andre0217@gmail.com>
2024-05-06 18:44:17 -0400

    removed unnecessary files; added config, routes, and controller directories; created config file with mysql variables

---
+++ server/config/config.ts
@@ -0,0 +1,16 @@
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const MYSQL_HOST = process.env.MYSQL_HOST || 'localhost';
+const MYSQL_DATABASE = process.env.MYSQL_DATABASE || 'bbr_database';
+const MYSQL_USER = process.env.MYSQL_USER || 'sandre';
+const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD || 'password';
+
+export const MYSQL = {
+    host: MYSQL_HOST,
+    db: MYSQL_DATABASE,
+    user: MYSQL_USER,
+    password: MYSQL_PASSWORD
+}
+
