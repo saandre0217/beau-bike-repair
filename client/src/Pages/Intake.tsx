728c7942e5082a51d0b5afc16c331cd802f8ec77
Sydney Andre <s.a.andre0217@gmail.com>
2024-05-12 10:11:56 -0500

    added frontend functionality to submit customer information to db; tested & working successfully

---
+++ client/src/Pages/Intake.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { CustomerInput } from '../Components/customerInput';
+
+export const Intake = () => {
+
+    return (
+        <div>
+            <CustomerInput />
+        </div>
+    )
+}
--- server/index.ts
+++ server/index.ts
@@ -4,7 +4,7 @@ import { connection, syncDatabase, createDatabase } from './config/db';
 import express, { Request, Response } from 'express'
 import customerRoutes from './routes/customers'
 import path from 'path'
-
+import cors from 'cors'
 const clientPath = path.resolve(__dirname, '../client/dist')
 export const app = express();
 
@@ -12,6 +12,10 @@ dotenv.config();
 
 app.use(express.json())
 app.use(express.static(clientPath))
+const corsOption = {
+  origin: ['http://localhost:3000'],
+};
+app.use(cors(corsOption));
 
 //route definition
 const routeHandler = express.Router()
