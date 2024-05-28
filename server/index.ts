db69e3e2207a297bba2718c7097682cd030c1d01
Sydney Andre <s.a.andre0217@gmail.com>
2024-05-28 13:41:00 -0500

    updated package.json type to module; updated tsconfig module and module resolution

--- server/index.ts
+++ server/index.ts
@@ -3,9 +3,12 @@ import dotenv from 'dotenv';
 import { connection, syncDatabase } from './config/db';
 import express from 'express'
 import routes from './routes'
-import path from 'path'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
 import cors from 'cors'
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const clientPath = path.resolve(__dirname, '../client/dist')
 export const app = express();
 
